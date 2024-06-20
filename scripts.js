29816fbc60548b52754c71da5c382ca1d1938415
da-nnn <166804251+da-nnn@users.noreply.github.com>
2024-06-20 16:02:57 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,24 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
   const portfolioSection = document.getElementById('portfolio');
   const reminderSection = document.getElementById('reminder');
 
-  calculatorLink.addEventListener('click', function() {
-    homeSection.classList.add('hidden');
-    calculatorSection.classList.remove('hidden');
-    portfolioSection.classList.add('hidden');
-    reminderSection.classList.add('hidden');
-  });
-
-  portfolioLink.addEventListener('click', function() {
-    homeSection.classList.add('hidden');
-    calculatorSection.classList.add('hidden');
-    portfolioSection.classList.remove('hidden');
-    reminderSection.classList.add('hidden');
-  });
-
-  reminderLink.addEventListener('click', function() {
-    homeSection.classList.add('hidden');
-    calculatorSection.classList.add('hidden');
-    portfolioSection.classList.add('hidden');
-    reminderSection.classList.remove('hidden');
-  });
-});
